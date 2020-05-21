aececbba95d5852fe633ada4ec63bb66b8cdb457
David Vique <davique@espe.edu.ec>
2020-05-21 15:40:26 -0500

    Creación del proyecto Mapeo y Versionamiento con git y repositorio de github

---
+++ SlnCertificacion0/PryCertificacion0/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PryCertificacion0
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
